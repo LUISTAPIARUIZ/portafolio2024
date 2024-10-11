43ecabde0cf951837b35a793ea4f6493bdfa35f4
Luis Tapia Ruiz <luistapiaruiz17@gmail.com>
2024-10-10 22:57:19 -0500

    Parte de proyectos terminado

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./styles/proyects.css"
 import imgCursosFemulp from "./img/Projects/CursosFemulp.png"
+import  imgFemulp from "./img/Projects/Femulp.png"
+import  imgWebConstruction from "./img/Projects/webConstruction1.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3Alt, faJsSquare, faPhp, faBootstrap } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5, faCss3Alt, faJsSquare, faPhp, faBootstrap ,faNodeJs,faReact} from '@fortawesome/free-brands-svg-icons'
 import { faDatabase, faEye, faTimes, faPlay } from '@fortawesome/free-solid-svg-icons'
 
 const Proyects = () => {
@@ -35,8 +37,8 @@ const Proyects = () => {
   const proyectos = [
     {
       imagen: imgCursosFemulp,
-      titulo: "Proyecto 1",
-      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      titulo: "Plataforma Cursos",
+      descripcion: "Plataforma de cursos virtuales con seguimiento de progreso y exámenes aleatorios. Esta plataforma está hecha en PHP puro y BD MySQL, y las tecnologías básicas de frontend. Inicialmente fue un desafío el modelo de negocio; sin embargo, logré superar ese reto y aquí se encuentra el resultado. Actualmente se encuentra en una etapa temprana.",
       enlace: "https://femulp.org/cursosFemulp/",
       tecnologias: [
         { icon: faHtml5, color: "#E34F26" },
@@ -45,6 +47,35 @@ const Proyects = () => {
         { icon: faPhp, color: "#777BB4" },
         { icon: faDatabase, color: "#4479A1" },
         { icon: faBootstrap, color: "#7952B3" }
+      ],
+      credenciales: {
+        correo: "visita@femulp.com",
+        contrasena: "visitaFemulp"
+      }
+    },
+    {
+      imagen: imgFemulp,
+      titulo: "FEMULP WEB",
+      descripcion: "Inicialmente, esta plataforma era una página estática. Con mis conocimientos básicos de backend y al identificar las necesidades del lugar donde realizaba mis prácticas, decidí darle dinamismo y una personalización adecuada para el público objetivo. Implementé formularios que enviaban correos personalizados para una mejor atención al público, con registros visibles en la administración. Uno de los mayores desafíos fue crear una sección de noticias totalmente dinámica y personalizable.",
+      enlace: "https://femulp.org",
+      tecnologias: [
+        { icon: faHtml5, color: "#E34F26" },
+        { icon: faCss3Alt, color: "#1572B6" },
+        { icon: faJsSquare, color: "#F7DF1E" },
+        { icon: faPhp, color: "#777BB4" },
+        { icon: faDatabase, color: "#4479A1" },
+        { icon: faBootstrap, color: "#7952B3" }
+      ]
+    },
+    {
+      imagen: imgWebConstruction,
+      titulo: "PNP BI En contruccion",
+      descripcion: "Esta plataforma está enfocada en centralizar el análisis de denuncias de la Policía Nacional del Perú. Este proyecto es uno de los retos más grandes a los que me estoy enfrentando. Pronto se encontrará disponible.",
+      enlace: "#",
+      tecnologias: [
+        { icon: faReact, color: "#2490DEFF" },
+        { icon: faNodeJs, color: "#19AE26FF" },
+        { icon: faDatabase, color: "#4479A1" },
       ]
     }
     // ... añade más proyectos aquí
@@ -70,6 +101,7 @@ const Proyects = () => {
               className={`ProyectsMain__img ${visibleProyectos.includes(index) ? 'visible' : ''}`}
             >
               <img className="ProyectsMain__img--img" src={proyecto.imagen} alt={proyecto.titulo} />
+              <h3 className="ProyectsMain__Titulo">{proyecto.titulo}</h3>
               <div className="ProyectsMain__overlay">
                 <FontAwesomeIcon icon={faEye} className="view-icon" onClick={() => abrirPopup(proyecto)} />
               </div>
@@ -88,32 +120,49 @@ const Proyects = () => {
         </div>
       </div>
       {popupAbierto && proyectoActual && (
-        <div className="ProyectsMain__popup">
-          <div className="ProyectsMain__popup-content">
-            <FontAwesomeIcon icon={faTimes} className="close-icon" onClick={cerrarPopup} />
-            <div className="popup-image-container">
-              <img src={proyectoActual.imagen} alt={proyectoActual.titulo} />
-              <div className="popup-info">
-                <h2>{proyectoActual.titulo}</h2>
-                <p>{proyectoActual.descripcion}</p>
-                <div className="popup-technologies">
-                  {proyectoActual.tecnologias.map((tech, i) => (
-                    <FontAwesomeIcon
-                      key={i}
-                      icon={tech.icon}
-                      className="tech-icon"
-                      style={{ color: tech.color }}
-                    />
-                  ))}
-                </div>
-                <a href={proyectoActual.enlace} target="_blank" rel="noopener noreferrer" className="ver-proyecto-btn">
-                  <FontAwesomeIcon icon={faPlay} /> Ver proyecto
-                </a>
-              </div>
+    <div className={`ProyectsMain__popup`}>
+      <div className={`ProyectsMain__popup-content open`}>
+        <FontAwesomeIcon icon={faTimes} className="close-icon" onClick={cerrarPopup} />
+        <div className="popup-image-container">
+          <img src={proyectoActual.imagen} alt={proyectoActual.titulo} />
+          <div className="popup-info">
+            <h2>{proyectoActual.titulo}</h2>
+            <p>{proyectoActual.descripcion}
+              <br />
+
+              {/* Validación si existen credenciales */}
+              {proyectoActual.credenciales && (
+                <ul>
+                  <li><strong>Correo:</strong> {proyectoActual.credenciales.correo}</li>
+                  <li><strong>Contraseña:</strong> {proyectoActual.credenciales.contrasena}</li>
+                </ul>
+              )}
+            </p>
+
+            <div className="popup-technologies">
+              {proyectoActual.tecnologias.map((tech, i) => (
+                // Si la tecnología es una imagen, la renderizamos como <img>
+                typeof tech.icon === 'string' ? (
+                  <img key={i} src={tech.icon} alt="tech" className="tech-image" style={{ color: tech.color, width: '30px', height: '30px' }} />
+                ) : (
+                  // Si es un ícono de FontAwesome, lo renderizamos como <FontAwesomeIcon>
+                  <FontAwesomeIcon
+                    key={i}
+                    icon={tech.icon}
+                    className="tech-icon"
+                    style={{ color: tech.color }}
+                  />
+                )
+              ))}
             </div>
+            <a href={proyectoActual.enlace} target="_blank" rel="noopener noreferrer" className="ver-proyecto-btn">
+              <FontAwesomeIcon icon={faPlay} /> Ver proyecto
+            </a>
           </div>
         </div>
-      )}
+      </div>
+    </div>
+  )}
     </section>
   )
 }
